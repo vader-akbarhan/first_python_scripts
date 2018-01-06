fc3914fc5417776e97a3ed514ef025258ca95ed9
Todor Stoychev <pretodor@gmail.com>
2018-01-06 22:55:42 +0200

    Refactor to use a functional approach

--- motivation1.py
+++ motivation1.py
@@ -1,40 +1,31 @@
-print("Value of __name__ is: ", __name__)
-if __name__=="__main__": 
-	print('Yes, the __name__ is "__main__". ')
+def show_salary_information(annual_salary):
 
-class Motivation(): 
-#	def __init__(self):
-#		print("The class Motivation was initiated.") 
+    monthly_salary = annual_salary // 12
+    daily_salary = monthly_salary // 21
+    hourly_salary = daily_salary // 8
 
-	def Rates(yearly_salary): 
-		yearly_salary = int(yearly_salary)
-		monthly = round((yearly_salary / 12), 0) 
-		daily = round(monthly / 21, 0) 
-		hourly = round(daily / 8, 0)
-		print("The monthly salary is {0}. \nThe daily salary is {1}. \nThe hourly salary is {2}".format(monthly, daily, hourly))
-	def Expertise(level = input('Type a level: "beginner", "intermediate" or "expert". ')): 
-		#
-		# level = input('Type a level: "beginner", "intermediate" or "expert". ')
-		# The line above produces the following error: 
-		# Type the required level: "beginner", "intermediate" or "expert". level
-		# Traceback (most recent call last):
-  		# File "motivation1.py", line 23, in <module> 
-    	# Motivation.Expertise(check_level)
-		# TypeError: Expertise() missing 1 required positional argument: 'level'
-		#
-		if level=="beginner": 
-			print("You can do it, hopefully, soon.")
-		elif level=="intermediate":
-			print("You MUST be heading towards such a level.")
-		elif level == "expert":
-			print("You would develop another profession before starting imaging happiness mediated by salary for expert IT skills.")
-		else: print("Type one of the levels.")
-		print("Globals: ", globals)
-		print("Locals: ", locals)
-# Motivation().Expertise()
-#check_salary = input("Enter the salary from the job offer: ")
-#Motivation.Rates(check_salary) 
+    print("Your monthly salary is {0}".format(monthly_salary))
+    print("Your daily salary is {0}".format(daily_salary))
+    print("Your hourly salary is {0}".format(hourly_salary))
 
-# check_level = input('Type the required level: "beginner", "intermediate" or "expert". ')
-# Motivation.Expertise(check_level)
-#Motivation.Expertise()
+
+def show_expertise_information(level):
+
+    if level == "beginner": 
+        print("You can do it, hopefully, soon.")
+    elif level == "intermediate":
+        print("You MUST be heading towards such a level.")
+    elif level == "expert":
+        print("You would develop another profession before starting imagining happiness mediated by salary for expert IT skills.")
+    else:
+        print("Unknown level.")
+
+
+if __name__=="__main__":
+
+    annual_salary = int(input('Type your annual salary (as an integer): '))
+    level = input('Type your level ("beginner", "intermediate" or "expert"): ')
+
+    show_salary_information(annual_salary)
+    print()
+    show_expertise_information(level)
