f89b99ea3fcc33d675423f9b58938fc4170d19b4
vader-akbarhan <35172134+vader-akbarhan@users.noreply.github.com>
2018-01-07 20:19:28 +0200

    Namespace issue; plan for the rest

--- roman_numerals.py
+++ roman_numerals.py
@@ -4,7 +4,7 @@
 
 #Let's first show the Roman numerals:
 #https://en.wikipedia.org/wiki/Roman_numerals
-#Symbol     I   V   X   L   C   D   M
+#Symbol I   V   X   L   C   D   M
 #Value  1   5   10  50  100 500 1000
 
 class All_Roads_Lead_To_Rome:   
@@ -32,4 +32,27 @@ if __name__ == '__main__':
     instance = All_Roads_Lead_To_Rome()
     decimal_list = instance.Extract_decimally() #WORKS! 
     tell = instance.Tell_me()
+
+#Work in progress. 
+#Error: 
+# Traceback (most recent call last):
+#  File "/media/user1/New Volume/IT studies/Python/roman_numerals.py", line 34, in <module>
+#    tell = instance.Tell_me()
+#  File "/media/user1/New Volume/IT studies/Python/roman_numerals.py", line 29, in Tell_me
+#    .format(*the_list)) #WORKS! And I used *args! # And I wrapped lines of code.
+#NameError: global name 'the_list' is not defined
+#
+#I am trying to track the namespaces so that I find what is wrong. Additionally: 
+# def Tell_me(self): 
+#        nonlocal the_list
+#        print(("The number is a sum of {} thousand(s), {} hundred(s), {} ten(s), and {} one(s). ")
+#        .format(*the_list)) #WORKS! And I used *args! # And I wrapped lines of code. 
+#
+# ... gives "SyntaxError: invalid syntax". I am still looking into the namespaces. 
+
+# After I fix the above issue, I will write a function (of the same class) which will substitute, 
+# say, 100 with "C" and "50" with "L". I intend to determine whether there is, say, 50 to be written, 
+# by dividing the corresponding number from "the_list" using modulo. For example: 90 % 50 = 40 -->
+# --> yes, write a "L" (50); then work with the remainder (40). 
+
 
